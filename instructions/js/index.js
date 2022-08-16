21ce99eb1c9d09a8016786c0e0e298a84d37e634
rfremp01 <rfremp01@tufts.edu>
2022-08-15 20:01:41 -0400

    add functionality with js and updated html file

--- instructions/js/index.js
+++ instructions/js/index.js
@@ -6,7 +6,7 @@ copyright.innerHTML = ` roselynn ${thisYear}`;
 footer.appendChild(copyright);
 
 const skills = ['Html,Css and Javascript','Drupal','Q|A with webdriversIO','Service Now'];
-let skillsSection = document.getElementById("#skills")
+let skillsSection = document.getElementById("skills")
 let skillList = skillsSection.querySelector('ul') //grabs ul element from #skills 
 
 for(let i = 0; i < skills.length; i ++){ 
@@ -15,3 +15,45 @@ for(let i = 0; i < skills.length; i ++){
     skillList.appendChild(skill)
 
 }
+
+//Handle Message Form Submit
+const messageForm = document.querySelector('form[name="leave_message"]');
+messageForm.addEventListener("submit", myFunction);
+
+//creating callback function
+function myFunction(){
+event.preventDefault();
+const eventTarget = event.target ;//event is the submit;  target is the form
+const firstInput = eventTarget.querySelector('input[name="name"]');
+console.log(firstInput.value);
+const secondInput = eventTarget.querySelector('input[type="email"]');
+console.log(secondInput.value);
+const thirdInput = eventTarget.querySelector('#message');
+console.log(thirdInput.value);
+
+
+//Display message in List
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.querySelector('ul');
+const newMessage = document.createElement("li");
+newMessage.innerHTML = `<a href="mailto: ${secondInput}">${firstInput}</a> wrote:  <span>${thirdInput}</span>`;
+
+
+const removeButton = document.createElement("button");
+    removeButton.innerText="remove";
+    removeButton.type="button";
+
+   removeButton.addEventListener("click", ()=>{
+     const entry = removeButton.parentNode;
+     entry.remove();
+    });
+
+   messageList.appendChild(newMessage);
+   newMessage.appendChild(removeButton);
+
+
+   messageSection.hidden = false;
+
+    messageForm.reset();
+};
+
